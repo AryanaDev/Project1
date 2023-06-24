2eb0ce98f8b33dcea4752cf734672d0f1f30ac5a
AryanaDev <aryanorasteh7@gmail.com>
2023-06-24 14:14:23 +0300

    select option metod in UniversityForms added

--- src/UniversityForm.js
+++ src/UniversityForm.js
@@ -1,46 +1,42 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 function UniversityForm() {
-  const [university, setUniversity] = useState([]);
-  const [item, setItem] = useState({ items: 10 });
-  const [data1, setData1] = useState([5, 25, 50, 100]);
+  const [university, setUniversity] = useState([])
+  const [item, setItem] = useState({ items: 10 })
+  const [allData, setAllData] = useState([])
 
   useEffect(() => {
     axios
       .get("http://universities.hipolabs.com/search?country=United+States")
       .then((response) => {
-        setUniversity(response.data.slice(0, 10));
-        console.log(response.data);
+        setUniversity(response.data.slice(0,item.items))
+        setAllData(response.data)
+        console.log(response.data)
       })
       .catch((error) => {
-        console.error(error);
+        console.error(error)
       });
-  }, []);
+  },[item.items]);
 
   useEffect(() => {
-    axios
-      .get("http://universities.hipolabs.com/search?country=United+States")
-      .then((response) => {
-        setUniversity(response.data.slice(0, [data1]));
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [data1]);
+    setAllData(allData)
+  },[allData])
 
   return (
     <>
-        <select   value={data1} onChange={(e) => setData1((e.target.value))}>
-          <option value={5}>5</option>
-          <option value={item.items}>10</option>
-          <option value={25}>25</option>
-          <option value={50}>50</option>
-          <option value={100}>100</option>
-        </select>
       <h1>UniversityForm</h1>
-      <h2>First {data1} University Names:</h2>
+      <h2>First {item.items} University Names:</h2>
+      <select
+        value={item.items}
+        onChange={(e) => setItem({ items: (e.target.value) })}
+      >
+        <option value={5}>5</option>
+        <option value={10}>10</option>
+        <option value={25}>25</option>
+        <option value={50}>50</option>
+        <option value={100}>100</option>
+      </select>
       <table style={{ borderCollapse: "collapse", width: "100%" }}>
         <thead>
           <tr>
